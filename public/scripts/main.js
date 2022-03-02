b5f4d618a0c143fcbfbac977ccaeb07dc4dc2ba9
jeffer <jeffersunde72@gmail.com>
2022-03-02 07:08:51 +0200

    Removing the # added in the link by the modal buttons

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -24,13 +24,15 @@ deleteButton.forEach(button => {
 })
 
 function handleClick(event, check = true) {
+    event.preventDefault()
+
     const text = check ? "Marcar como lida" : "Excluir"
     modalTitle.innerHTML =  `${text} esta pergunta`
     modalDescription.innerHTML = `Tem certeza que deseja ${text.toLocaleLowerCase()} esta pergunta?`
     modalButton.innerHTML = `Sim, ${text.toLocaleLowerCase()}`
 
     check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
-    
+
     // Abrir modal
     modal.open()
 }
