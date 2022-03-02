feb3cb95a6f641b0d991bf3fd6bbdd27ab853efb
jeffer <jeffersunde72@gmail.com>
2022-03-02 07:04:22 +0200

    Fixed the modal aspect and created other functions

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,18 +1,17 @@
 import Modal from "./modal.js"
 
 const modal = Modal()
+const modalTitle = document.querySelector(".modal h2")
+const modalDescription = document.querySelector(".modal p")
+const modalButton = document.querySelector(".modal button")
 
 // Pegar todos os butões que existe com a classe check
 const checkButtons = document.querySelectorAll(".actions a.check")
 
 // Adicionar o ouvidor de eventos para todos os botões com a classe check
 checkButtons.forEach(button => {
-
     // Adicionar a escuta
-    button.addEventListener("click", event => {
-        // Abrir modal
-        modal.open()
-    })
+    button.addEventListener("click", handleClick)
 })
 
 // Pegar todos os butões que existe com a classe delete
@@ -20,10 +19,18 @@ const deleteButton = document.querySelectorAll(".actions a.delete")
 
 // Adicionar o ouvidor de eventos para todos os botões com a classe delete
 deleteButton.forEach(button => {
-
     // Adicionar a escuta
-    button.addEventListener("click", event => {
-        // Abrir modal
-        modal.open()
-    })
+    button.addEventListener("click", event => handleClick(event, false))
 })
+
+function handleClick(event, check = true) {
+    const text = check ? "Marcar como lida" : "Excluir"
+    modalTitle.innerHTML =  `${text} esta pergunta`
+    modalDescription.innerHTML = `Tem certeza que deseja ${text.toLocaleLowerCase()} esta pergunta?`
+    modalButton.innerHTML = `Sim, ${text.toLocaleLowerCase()}`
+
+    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+    
+    // Abrir modal
+    modal.open()
+}
